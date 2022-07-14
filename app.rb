3ac60a1102b3287d80b2d023576275d6798f6530
duvan <duvanfoz@hotmail.com>
2022-07-14 12:04:40 -0500

    added cockbook

--- app.rb
+++ app.rb
@@ -8,6 +8,37 @@ configure :development do
   BetterErrors.application_root = File.expand_path(__dir__, __FILE__)
 end
 
+require_relative 'cookbook'
+require_relative 'recipe'
+
 get '/' do
-  'Hello world!'
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  @recipes = cookbook.all
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/recipes' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  recipe = Recipe.new(
+    name: params[:name],
+    description: params[:description],
+    rating: params[:rating],
+    prep_time: params[:prep_time]
+  )
+  cookbook.add_recipe(recipe)
+  redirect to '/'
+end
+
+get '/recipes/:index' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  cookbook.remove_recipe(params[:index].to_i)
+  redirect to '/'
 end
---
+++ cookbook.rb
@@ -0,0 +1,49 @@
+require 'csv'
+require_relative 'recipe'
+
+class Cookbook
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @recipes = []
+    load_data
+  end
+
+  def all
+    return @recipes
+  end
+
+  def find(index)
+    @recipes[index]
+  end
+
+  def add_recipe(recipe)
+    @recipes << recipe
+    save_data
+  end
+
+  def remove_recipe(recipe_index)
+    @recipes.delete_at(recipe_index)
+    return @recipes
+  end
+
+  def load_data
+    CSV.foreach(@csv_file_path) do |row|
+      row[4] = row[4] == 'true'
+      recipe = Recipe.new(name: row[0], description: row[1], rating: row[2], prep_time: row[3], done: row[4])
+      @recipes << recipe
+    end
+  end
+
+  def save_data
+    CSV.open(@csv_file_path, 'wb') do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe.name, recipe.description, recipe.rating, recipe.prep_time, recipe.done?]
+      end
+    end
+  end
+
+  def destroy_all
+    @recipes = []
+    save_data
+  end
+end
